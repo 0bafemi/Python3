a404e363570dcfdedeab7ec669baa6d97116a5ce
Obafemi Emmanuel Oduola <85515694+0bafemi@users.noreply.github.com>
2025-12-03 15:11:32 -0500

    Update path_planner_cli.py

--- path_planner_cli.py
+++ path_planner_cli.py
@@ -1,6 +1,6 @@
 import argparse
 from src.graph import GridGraph, Cell
-from src.graph_search import a_star_search, breadth_first_search, depth_first_search
+from src.graph_search import a_star_search
 from src.utils import generate_plan_file
 
 
@@ -9,11 +9,9 @@ def parse_args():
     parser.add_argument("-m", "--map", type=str, required=True, help="Path to the map file.")
     parser.add_argument("--start", type=int, nargs=2, required=True, help="Start cell.")
     parser.add_argument("--goal", type=int, nargs=2, required=True, help="Goal cell.")
-    parser.add_argument("--algo", type=str, default="bfs", choices=["bfs", "dfs", "astar"],
-                        help="Algorithm to use.")
 
+    # We no longer expose an algorithm choice; we always use A*.
     args = parser.parse_args()
-
     return args
 
 
@@ -25,16 +23,8 @@ if __name__ == "__main__":
     # Construct the start and goal cells.
     start, goal = Cell(*args.start), Cell(*args.goal)
 
-    # Run the planning algorithm of the user's choice.
-    if args.algo == "astar":
-        path = a_star_search(graph, start, goal)
-    elif args.algo == "bfs":
-        path = breadth_first_search(graph, start, goal)
-    elif args.algo == "dfs":
-        path = depth_first_search(graph, start, goal)
-    else:
-        print("Invalid option:", args.algo)
-        exit()
+    # Always run A* search.
+    path = a_star_search(graph, start, goal)
 
     # Output the planning file for visualization.
-    generate_plan_file(graph, start, goal, path, algo=args.algo)
+    generate_plan_file(graph, start, goal, path, algo="astar")
